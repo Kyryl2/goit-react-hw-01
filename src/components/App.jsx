b6e649a9caa60ef4863aadb5026ad0e3eb8a34a8
Kyryl2 <krasiuk.4884@gmail.com>
2024-04-28 16:41:42 +0200

    react done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,9 @@
 import Profile from "./Profile/Profile";
+import FriendList from "./FriendList/FriendList";
+import TransactionHistory from "./TransactionHistory/TransactionHistory";
 import userData from "../userData.json";
+import friends from "../friends.json";
+import transactions from "../transactions.json";
 import "./App.css";
 
 const App = () => {
@@ -12,6 +16,8 @@ const App = () => {
         image={userData.avatar}
         stats={userData.stats}
       />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </>
   );
 };
